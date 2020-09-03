cf97966311285ae8c811a71ae1dcdfe8be933bef
Nozomi Hasegawa <hase.64.34@gmail.com>
2020-09-03 17:36:00 +0900

    add template.cpp

---
+++ template.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define REP(i, n) for(int i = 0; i < (int)(n); i++)
+#define IN(type, n) type n; cin >> n
+
+int main() {
+  
+
+  return 0;
+}
