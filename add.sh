9fba34952f029994c9418b99c9905a674b51e9e7
Nozomi Hasegawa <hase.64.34@gmail.com>
2020-09-04 19:14:30 +0900

    add.sh support atcoder, AOJ, yukicoder

--- add.sh
+++ add.sh
@@ -9,7 +9,7 @@ function get_atcoder() {
     declare tmp_status=$(curl -Ls ${url_pre}a -o /dev/null -w '%{http_code}\n')
     if [ ${tmp_status} -eq 404 ]; then
       if [[ ${url_pre} =~ "beginner" ]]; then
-        url_pre=${url_pre%%-beginner*}
+        url_pre=${url_pre%%_beginner*}
         url_pre+="_"
       fi
       tmp_status=$(curl -Ls ${url_pre}a -o /dev/null -w '%{http_code}\n')
@@ -18,6 +18,12 @@ function get_atcoder() {
       declare url_in
       url_pre="${url_pre%\/*}/"
       read -p "URL? > ${url_pre}" url_in
+      if [ "${url_in: -1}" != "_" ]; then
+        if [[ "${url_in: -2}" =~ _[a-z] ]]; then
+          url_in=${url_in%_*}
+        fi
+        url_in+="_"
+      fi
       url_pre+=${url_in}
     fi
   else
@@ -29,14 +35,14 @@ function get_atcoder() {
 
   for name in ${argv[@]}; do
     if [ -f ${name}.in ]; then
-      # rm ${name}.in  # for debug
-      echo "[add] ${name}.in already exists"
-      continue
+      if "${rflag}"; then
+        rm ${name}.in
+      else
+        echo "[add] ${name}.in already exists"
+        continue
+      fi
     fi
 
-    # if [ ${name} = "test" ]; then
-    #   url="https://atcoder.jp/contests/abc177/tasks/abc177_d"
-    # fi
     declare url=${url_pre}
     url+=${name}
     declare url2=${url_pre}
@@ -71,8 +77,7 @@ function get_atcoder() {
           inflag=false
           echo "</in>" >> ${name}.in
         else
-          declare tmp=${line}
-          echo -e "${tmp}" >> ${name}.in
+          echo ${line} >> ${name}.in
         fi
       elif "${outflag}"; then
         if [[ ${line} =~ "</pre>" ]]; then
@@ -99,14 +104,173 @@ END
 }
 
 function get_aoj() {
-  declare url_pre="http://judge.u-aizu.ac.jp/onlinejudge/finder.jsp?course=DPL"
+  declare url_pre="http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id="
+  if [ ${path[1]} = "course" ]; then
+    url_pre+="${path[2]}_"
+  elif [ ${path[1]} = "contest" ]; then
+    echo "[add] Not supported"
+    exit 1
+  fi
+
+  for name in ${argv[@]}; do
+    if [ -f ${name}.in ]; then
+      if "${rflag}"; then
+        rm ${name}.in
+      else
+        echo "[add] ${name}.in already exists"
+        continue
+      fi
+    fi
+
+    declare url=${url_pre}
+    url+=$(echo ${name} | tr "[a-z]" "[A-Z]")
+    declare status=$(curl -Ls ${url} -o /dev/null -w '%{http_code}\n')
+    if [ ${status} -ne 200 ]; then
+      echo "[add] Cannot recieve contents from ${url} (code: ${status})"
+      continue
+    fi
+
+    declare data=$(curl -Lso- ${url})
+    data=${data#*<[hH]1>}
+    data=${data%%<h3 style="color:#aaa">Note</h3>*}
+    declare inflag1=false
+    declare inflag2=false
+    declare outflag1=false
+    declare outflag2=false
+    touch ${name}.in
+  
+    echo "[add] Get from ${url}"
+    while read line; do
+      line=`echo ${line} | tr -d "\r"`
+      if "${inflag1}"; then
+        if [[ ${line} =~ "<pre>" ]]; then
+          inflag1=false
+          inflag2=true
+        fi
+      elif "${inflag2}"; then
+        if [[ ${line} =~ "</pre>" ]]; then
+          inflag2=false
+          echo "</in>" >> ${name}.in
+        else
+          echo ${line} >> ${name}.in
+        fi
+      elif "${outflag1}"; then
+        if [[ ${line} =~ "<pre>" ]]; then
+          outflag1=false
+          outflag2=true
+        fi
+      elif "${outflag2}"; then
+        if [[ ${line} =~ "</pre>" ]]; then
+          outflag2=false
+          echo "</out>" >> ${name}.in
+        else
+          echo ${line} >> ${name}.in
+        fi
+      elif [[ ${line} =~ "2>Sample Input" ]] || [[ ${line} =~ "3>入力例" ]]; then
+        echo "<in>" >> ${name}.in
+        inflag1=true
+      elif [[ ${line} =~ "2>Sample Output" ]] || [[ ${line} =~ "3>出力例" ]] || [[ ${line} =~ "2>Output for the Sample Input" ]]; then
+        echo "<out>" >> ${name}.in
+        outflag1=true
+      fi
+    done << END
+    ${data}
+END
+
+    echo "[add] Added ${name}.in"
+  done
+}
+
+function get_yukicoder() {
+  declare url_pre="https://yukicoder.me/problems/no/"
+
+  for name in ${argv[@]}; do
+    if [ -f ${name}.in ]; then
+      if "${rflag}"; then
+        rm ${name}.in
+      else
+        echo "[add] ${name}.in already exists"
+        continue
+      fi
+    fi
+
+    declare url=${url_pre}
+    url+=${name}
+    declare status=$(curl -Ls ${url} -o /dev/null -w '%{http_code}\n')
+    if [ ${status} -ne 200 ]; then
+      echo "[add] Cannot recieve contents from ${url} (code: ${status})"
+      continue
+    fi
+
+    declare data=$(curl -Lso- ${url})
+    data=${data#*<h4 class=\"shadow\">サンプル</h4>}
+    data=${data%%<input type=\"hidden\" name=\"csrf_token\"*}
+    declare inflag=false
+    declare outflag=false
+    touch ${name}.in
+  
+    echo "[add] Get from ${url}"
+    while read line; do
+      line=`echo ${line} | tr -d "\r"`
+      if "${inflag}"; then
+        if [[ ${line} =~ "<pre>" ]]; then
+          if [[ ${line} =~ "</pre>" ]]; then
+            inflag=false
+          fi
+          line=$(echo "${line#*<pre>}" | sed s/\<br\ \\/\>/\\$'\n'/g | sed "s/<\\/pre>//g")
+          echo "${line}" >> ${name}.in
+          if !("${inflag}"); then
+            echo "</in>" >> ${name}.in
+          fi
+        elif [[ ${line} =~ "</pre>" ]]; then
+          inflag=false
+          echo "</in>" >> ${name}.in
+        else
+          echo ${line} >> ${name}.in
+        fi
+      elif "${outflag}"; then
+        if [[ ${line} =~ "<pre>" ]]; then
+          if [[ ${line} =~ "</pre>" ]]; then
+            outflag=false
+          fi
+          line=$(echo "${line#*<pre>}" | sed s/\<br\ \\/\>/\\$'\n'/g | sed "s/<\\/pre>//g")
+          echo "${line}" >> ${name}.in
+          if !("${outflag}"); then
+            echo "</out>" >> ${name}.in
+          fi
+        elif [[ ${line} =~ "</pre>" ]]; then
+          outflag=false
+          echo "</out>" >> ${name}.in
+        else
+          echo ${line} >> ${name}.in
+        fi
+      elif [[ ${line} =~ "<h6>入力</h6>" ]]; then
+        echo "<in>" >> ${name}.in
+        inflag=true
+      elif [[ ${line} =~ "<h6>出力</h6>" ]]; then
+        echo "<out>" >> ${name}.in
+        outflag=true
+      fi
+    done << END
+    ${data}
+END
+
+    echo "[add] Added ${name}.in"
+  done
 }
 
 
 declare -i argc=0
 declare -a argv=()
+declare rflag=false
 while test "$1" != ""; do
   case $1 in
+    -*)
+      if [[ "$1" =~ 'r' ]] || [[ "$1" =~ '-remove' ]]; then
+        rflag=true
+      fi
+      shift
+      ;;
     *)
       ((++argc))
       argv=("${argv[@]}" "${1%.cpp}")
@@ -129,6 +293,12 @@ declare path=( $(pwd | sed -e "s/.*competitive_programming\///g" | tr "/" " ") )
 case ${path[0]} in
   "atcoder")
     get_atcoder
+    ;;
+  "AOJ")
+    get_aoj
+    ;;
+  "yukicoder")
+    get_yukicoder
     ;;
   *)
     echo "[add] ${path[0]} is unsupported"
