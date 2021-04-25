484fa62de9c150ac5bae7a5b0923993a5bfe5730
Nozomi Hasegawa <hase.64.34@gmail.com>
2021-04-25 15:11:26 +0900

    solve ABC187 F

--- atcoder/ABC/187/f.cpp
+++ atcoder/ABC/187/f.cpp
@@ -29,79 +29,131 @@ void yes() { cout << "Yes" << endl; }
 void no() { cout << "No" << endl; }
 #pragma endregion
 
+int pow2(int n) {
+  assert(n < 32);
+  int ans = 1;
+  REP(i, n) {
+    ans *= 2;
+  }
+
+  return ans;
+}
+
+vector<int> get_neibors(int target, vector<vector<bool> > &edges){
+  vector<int> neibors;
+  REP(i, edges.at(target).size()) {
+    if (edges.at(target).at(i)) neibors.emplace_back(i);
+  }
+
+  return neibors;
+}
+
+bool is_group_ok(vector<int> &group, vector<vector<bool> > &edges) {
+  if (group.size() == 1) return true;
+
+  for (auto const &target : group) {
+    for (auto const &member : group) {
+      if (member == target) continue;
+      if (!edges.at(target).at(member)) return false;
+    }
+  }
+
+  return true;
+}
+
+vector<vector<int> > get_max_groups(int target, vector<vector<bool> > &edges) {
+  vector<vector<int> > max_groups;
+  vector<int> neibors = get_neibors(target, ref(edges));
+  if (neibors.size() == 0) return max_groups;
+
+  int max_size = 0;
+  REP(i, pow2((int)neibors.size())) {
+    vector<int> group;
+    group.emplace_back(target);
+    REP(j, neibors.size()) {
+      if ((i >> j) & 1) {
+        group.emplace_back(neibors.at(j));
+      }
+    }
+    if (!is_group_ok(ref(group), ref(edges))) continue;
+    if ((int)group.size() < max_size) continue;
+    if ((int)group.size() > max_size) {
+      max_size = (int)group.size();
+      max_groups.clear();
+    }
+    max_groups.emplace_back(group);
+  }
+
+  return max_groups;
+}
+
+int get_min_edge_node(vector<vector<bool> > &edges, vector<bool> &checked) {
+  int ans = (int)edges.size();
+  int min = ans * ans;
+  REP(i, edges.size()) {
+    if (checked.at(i)) continue;
+    int count = 0;
+    REP(j, edges.at(i).size()) {
+      if (edges.at(i).at(j)) count++;
+    }
+    if (chmin(min, count)) ans = i;
+  }
+
+  return ans;
+}
+
+int solve(int N, int target, vector<vector<bool> > &edges, vector<bool> &checked) {
+  if (target >= N) return 0;
+
+  vector<vector<int> > max_groups = get_max_groups(target, ref(edges));
+  if (max_groups.size() == 0) {
+    checked.at(target) = true;
+    return 1 + solve(N, get_min_edge_node(ref(edges), ref(checked)), ref(edges), ref(checked));
+  }
+
+  int ans = N;
+  for (auto const &group : max_groups) {
+    vector<vector<bool> > new_edges = edges;
+    for (auto const &member : group) {
+      REP(k, new_edges.at(member).size()) {
+        new_edges.at(member).at(k) = false;
+        new_edges.at(k).at(member) = false;
+      }
+    }
+
+    vector<bool> new_checked = checked;
+    for (auto const &member : group) {
+      assert(new_checked.at(member) == false);
+      new_checked.at(member) = true;
+    }
+
+    chmin(ans, solve(N, get_min_edge_node(ref(new_edges), ref(new_checked)), ref(new_edges), ref(new_checked)));
+  }
+
+  assert (ans < N);
+  return ans + 1;
+}
+
 int main() {
   IN(int, N);
   IN(int, M);
-  vector<vector<int> > node(N);
-  vector<vector<int> > group;
+
+  vector<vector<bool> > edges(N, vector<bool>(N, false));
   REP(i, M) {
     IN(int, A);
-    A--;
     IN(int, B);
-    B--;
-    node.at(A).emplace_back(B);
-    node.at(B).emplace_back(A);
+    A--; B--;
+    edges.at(A).at(B) = true;
+    edges.at(B).at(A) = true;
   }
 
-  // REP(i, N) {
-  //   cout << "node" << i << ": ";
-  //   for (auto n : node.at(i)) {
-  //     cout << n << ", ";
-  //   }
-  //   cout << endl;
-  // }
-
-  vector<pair<int, int> > sizes(N);
-  REP(i, N) {
-    sizes.at(i) = make_pair((int)node.at(i).size(), i);
+  int ans = N;
+  REP(j, N) {
+    vector<vector<bool> > new_edges = edges;
+    vector<bool> checked(N, false);
+    chmin(ans, solve(N, j, ref(new_edges), ref(checked)));
   }
-  sort(ALL(sizes), greater<pair<int, int> >());
-
-  // REP(i, N) {
-  //   cout << sizes.at(i).first << ", " << sizes.at(i).second << endl;
-  // }
-
-  REP(i, N) {
-    int idx = sizes.at(i).second;
-    // REP(j, (int)group.size()) {
-    //   cout << "groupe" << j << ": ";
-    //   REP(k, (int)group.at(j).size()) {
-    //     cout << group.at(j).at(k) << ", ";
-    //   }
-    //   cout << endl;
-    // }
-    // cout << "idx: " << idx << endl;
-    bool new_flag = true;
-    REP(j, (int)group.size()) {
-      bool add_flag = true;
-      REP(k, (int)group.at(j).size()) {
-        int tmp_node = group.at(j).at(k);
-        // cout << "tmp_node: " << tmp_node << endl;
-        bool ok_flag = false;
-        for (int n : node.at(tmp_node)) {
-          if (n == idx) {
-            ok_flag = true;
-          }
-        }
-        if (!ok_flag) {
-          add_flag = false;
-        }
-      }
-      if (add_flag) {
-        group.at(j).emplace_back(idx);
-        new_flag = false;
-        break;
-      }
-    }
-
-    if (new_flag) {
-      // cout << "new" << endl;
-      vector<int> v(1, idx);
-      group.emplace_back(v);
-    }
-  }
-
-  cout << group.size() << endl;
+  cout << ans << endl;
 
   return 0;
 }
